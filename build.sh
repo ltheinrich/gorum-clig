721c93c4899063e90ae9ddd47dfd7e1e2d17f565
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-13 14:03:01 +0100

    sign after build

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# linux amd64
+# build binaries and sign
+rm -rf bin
 GOOS=linux GOARCH=amd64 go build -o bin/clig-linux-amd64
-
-# freebsd amd64
 GOOS=freebsd GOARCH=amd64 go build -o bin/clig-freebsd-amd64
+cd bin
+gpg2 -a --detach-sign clig-linux-amd64
+gpg2 -a --detach-sign clig-freebsd-amd64
