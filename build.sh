548376c3b3e7894291ddfa064c1993f5f203847f
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-14 15:12:33 +0100

    binary and signature in root on build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf bin; GOOS=linux GOARCH=amd64 go build -o bin/clig
-cd bin; gpg2 -a --detach-sign clig
+rm -f clig; rm -f clig.asc
+GOOS=linux GOARCH=amd64 go build && gpg2 -a --detach-sign clig
