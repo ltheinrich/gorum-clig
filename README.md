9ab68a47a9a9e3ecb7cc153cf204de6e9a2dd991
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-14 15:02:12 +0100

    only linux (amd64) builds by default and add bcrypt vendor

--- README.md
+++ README.md
@@ -3,12 +3,6 @@ Command-line interface for Gorum
 
 ## Get list of arguments and commands/actions
 
-### Linux (amd64)
 ```
-./clig-linux-amd64 -help
-```
-
-### FreeBSD (amd64)
-```
-./clig-freebsd-amd64 -help
+./clig -help
 ```
--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-rm -rf bin
-GOOS=linux GOARCH=amd64 go build -o bin/clig-linux-amd64
-GOOS=freebsd GOARCH=amd64 go build -o bin/clig-freebsd-amd64
-cd bin; gpg2 -a --detach-sign clig-linux-amd64 && gpg2 -a --detach-sign clig-freebsd-amd64
+rm -rf bin; GOOS=linux GOARCH=amd64 go build -o bin/clig
+cd bin; gpg2 -a --detach-sign clig
